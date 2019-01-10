cd88610e1a524fbabe79ff7aa4a004064509a6cc
rahulthakkar796 <rahulthakkar796@gmail.com>
2019-01-10 15:20:37 +0530

    stable with half working timer

--- app/js/components/Milestone.vue
+++ app/js/components/Milestone.vue
@@ -24,27 +24,29 @@
         </v-layout>
           </v-container>
           <v-container class="mt-2 mb-3">
-            <v-layout row wrap justify-center>
+            <v-layout row wrap justify-center align-center>
               <v-flex xs12 md12 lg6>
             <v-card dark flat class="mt-2 mb-2 mileList" v-for="(detail,index) in details" :key="index">
              
               <v-layout row wrap justify-center>
-                <v-flex xs12 md6>
+                <v-flex xs12 md6 lg12>
                   <h1 class="pa-2 font-weight-medium">ID:{{index+1}}</h1>
+                  <hr class="pa-1">
+                 
                   
-                  <hr>
-                  
-                  <h2 class="pa-2">Title:{{detail.title}}</h2>
+                  <h2 class="pa-2 font-weight-light">Title:{{detail.title}}</h2>
+                </v-flex>
+                </v-layout>
+                
+               
+                <v-layout row wrap justify-center align-center>
+                <v-flex xs12 md6>
+                  <h2 class="pa-2 font-weight-light">Price:{{detail.price}}</h2>
                 </v-flex>
                 </v-layout>
                 <v-layout row wrap justify-center>
                 <v-flex xs12 md6>
-                  <h2 class="pa-2">Price:{{detail.price}}</h2>
-                </v-flex>
-                </v-layout>
-                <v-layout row wrap justify-center>
-                <v-flex xs12 md6>
-                  <h2 class="pa-2">Duration:{{detail.duration}}</h2>
+                  <h2 class="pa-2 font-weight-light">Duration:{{detail.duration}}</h2>
                 </v-flex>
                 </v-layout>
                 <v-layout row wrap justify-center>
@@ -52,33 +54,39 @@
                   <v-chip outline class="pa-2 heading" text-color="white">Status:{{detail.status}}</v-chip>
                 </v-flex> 
                 </v-layout>
+                 <v-layout row wrap justify-center v-if="detail.status=='Activated' && detail.mins>=0 && detail.secs>=0">
+                <v-flex xs12 md6>
+                  <v-chip outline class="pa-2 heading" text-color="white">Time Remaining:{{detail.mins}} Mins , {{detail.secs}} secs</v-chip>
+                </v-flex> 
+                </v-layout>
+                
                  <v-card-actions>
-                 <v-layout row wrap justify-center>
+                 <v-layout row wrap justify-center align-center>
                   
-                    <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="activate(index)">Activate Milestone</v-btn>
+                    <v-flex xs8 md4>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="activate(index)" :disabled="details[index].status=='Activated'">Activate Milestone</v-btn>
                     </v-flex>
                  
-                   <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="completeBuyer(index)">Complete by Buyer</v-btn>
+                   <v-flex xs8 md4>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="completeBuyer(index)">Complete by Buyer</v-btn>
                    </v-flex>
                    
                    <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="cancelBuyer(index)">Cancel by Buyer</v-btn>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="cancelBuyer(index)">Cancel by Buyer</v-btn>
                    </v-flex>
                     
                     <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="requestBuyer(index)">Request by Buyer</v-btn>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="requestBuyer(index)">Request by Buyer</v-btn>
                     </v-flex>
                      
                      <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="completeSeller(index)">Complete by Seller</v-btn>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="completeSeller(index)">Complete by Seller</v-btn>
                      </v-flex>
                      <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" @click.prevent="cancelSeller(index)">Cancel by Seller</v-btn>
+                    <v-btn flat outline class="mt-2 btn" block @click.prevent="cancelSeller(index)">Cancel by Seller</v-btn>
                     </v-flex>
                     <v-flex xs8 s3 md4>
-                    <v-btn flat outline class="mt-2" center @click.prevent="requestSeller(index)">Request by Seller</v-btn>
+                    <v-btn flat outline class="mt-2 btn" block center @click.prevent="requestSeller(index)">Request by Seller</v-btn>
                     </v-flex>
 
                      
@@ -114,6 +122,8 @@ export default {
       obj:null,
       details:Array(),
       currentStatus:'',
+      mins:0,
+      secs:0,
       load:false,
       status:['Milestone created','Activated','Completed By Seller','Completed By Both','Canceled By Seller','Canceled By Buyer','Requested By Seller','Requested By Buyer']
     }
@@ -127,16 +137,54 @@ export default {
           let p=await Milestone.methods.getPrice(this.noMilestones-1).call();
           let d=await Milestone.methods.getDuration(this.noMilestones-1).call();
           this.currentStatus=await Milestone.methods.getStatus(this.noMilestones-1).call();
-          this.obj={title:t,price:p,duration:d,status:this.status[this.currentStatus]};
+          this.obj={title:t,price:p,duration:d,status:this.status[this.currentStatus],mins:0,secs:0};
           this.details.push(this.obj);
           this.load=false;
         
         //await this.storeIn(this.noMilestones);
       },
+      timer:function(index){
+        let now=new Date(Date.now());
+        now=(now.getTime())/1000;
+        let time=(this.details[index].duration)*60;
+        let totalTime=(time+now);
+        let timee=setInterval(()=>{
+      
+        console.log("time:"+time);
+        console.log("now:"+now);
+      
+        let min=new Date(Date.now());
+        min=(min.getTime())/1000;
+        let remainTime=totalTime-min;
+        //remainTime=parseInt(remainTime/1000);
+
+        let day=parseInt(remainTime/86400);
+       console.log("day:"+day);
+       remainTime=(remainTime%86400);
+       let hour=parseInt(remainTime/3600);
+       console.log("hour:"+hour);
+        remainTime=(remainTime%3600);
+        this.mins=parseInt(remainTime/60);
+        console.log("mins:"+this.mins);
+        remainTime=(remainTime%60);
+        this.secs=parseInt(remainTime);
+        console.log("secs:"+this.secs);
+        if(remainTime<0){
+          console.log("Time's up!");
+          clearInterval(timee);
+        }
+
+        this.details[index].mins=this.mins;
+        this.details[index].secs=this.secs;
+        },500
+        )
+      },
       activate:async function(no){
         await Milestone.activateMilestone(no);
+        this.timer(no);
         this.getStatus(no);
-        alert('Milestone activated!');
+       // alert('Milestone activated!');
+        
       },
       completeBuyer:async function(index) {
         await Milestone.completeMilestoneBuyer(index);
@@ -172,7 +220,7 @@ export default {
       },
       getStatus:async function(index) {
         this.currentStatus=await Milestone.methods.getStatus(index).call();
-        this.obj.status=this.status[this.currentStatus];
+        this.details[index].status=this.status[this.currentStatus];
       },
      
       storeIn:async function(no){
@@ -181,8 +229,17 @@ export default {
           let p=await Milestone.methods.getPrice(i).call();
           let d=await Milestone.methods.getDuration(i).call();
           this.currentStatus=await Milestone.methods.getStatus(i).call();
-          this.obj={title:t,price:p,duration:d,status:this.status[this.currentStatus]};
+          if(this.currentStatus==1){
+          
+          this.obj={title:t,price:p,duration:d,status:this.status[this.currentStatus],mins:this.mins,secs:this.secs};
           this.details.push(this.obj);
+          this.timer(i);
+          }
+          else{
+              this.obj={title:t,price:p,duration:d,status:this.status[this.currentStatus],mins:this.mins,secs:this.secs};
+              this.details.push(this.obj);
+          }
+          
         }
         
       }
@@ -228,6 +285,10 @@ export default {
 <style scoped>
 .cardLayout,.mileList{
   border-radius: 15px;
+}
+.btn{
+  /* width:180px; */
+  
 }
 
 </style>
